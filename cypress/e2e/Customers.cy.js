b5f1cfb4d3fc772b7831ae47337d465f8d9093b4
Dario <darjo@motomtech.com>
2025-05-29 17:16:40 +0200

    Customer Changes

--- cypress/e2e/Customers.cy.js
+++ cypress/e2e/Customers.cy.js
@@ -1,5 +1,15 @@
 const TEST_DATA = {
     baseUrl: 'https://admin-stg.onehomesolution.com/customers/list',
+    name: 'darjo',
+    last: 'tester',
+    phone: '2225555555',
+    emailDomain: '@motomtech.com',
+    company: 'Tester',
+    lead: 'Test',
+    city: 'Salt Lake City',
+    state: 'UT',
+    zipCode: '84105',
+    nameProperty: 'Tester'
   };
 
 const SELECTORS = { 
@@ -17,11 +27,20 @@ const SELECTORS = {
     propertyState: 'input[name="state"]',
     propertyZipCode: 'input[name="zip_code"]',
     successToast: '.toaster > .group',
-
+    propetyName: 'input[placeholder="Name"]',
+    propertyLocation: 'button[role="combobox"]',
+    propetyOption: '[role="option"]',
+    tableRow: 'tr.cursor-pointer',
+    deleteMenu: 'button[aria-haspopup="menu"]',
+    menuItem: '[role="menuitem"]',
+    
 }
 const MESSAGES = {
     manualAddressButton: 'Add an address manually',
     successfulToast: 'Successfully created',
+    locationSelector: 'Select location',
+    deleteOption: 'Delete',
+    
   };
 
 const navigateToCustomerPage = () => {
@@ -29,23 +48,149 @@ const navigateToCustomerPage = () => {
     cy.wait(3000);
   };
 
-  // Calling a random number
-//   cy.generateRandomNumber().then((number) => {
-//     console.log('Random number:', number); // e.g., "3847592016"
-//     cy.get('input[name="phone"]').type(number);
-//   });
+  const createCustomerProfile = () => {
+   cy.get(SELECTORS.createCustomerButton)
+         .click();
 
-  // Generating 5 random strings
-// cy.generateRandomWords().then((words) => {
-//     console.log('Random words:', words);
-//     cy.get('input[name="first_name"]').type(words.join(' ')); // Types all words with spaces
-//   });
+    cy.generateRandomNumber(10).then((randomNumber) => {
+          const dynamicName = `${TEST_DATA.name}+${randomNumber}`;
+          console.log('Generated name:', dynamicName);
+   
+    cy.get(SELECTORS.firstName)
+         .click()
+         .type(dynamicName);
+         })
 
+    cy.get(SELECTORS.lastName)
+         .click()
+         .type(TEST_DATA.last);
+
+    cy.get(SELECTORS.phoneNumber)
+         .click()
+         .type(TEST_DATA.phone);
+
+    cy.generateRandomNumber(10).then((randomNumber) => {
+          const dynamicEmail = `${TEST_DATA.name}+${randomNumber}${TEST_DATA.emailDomain}`;
+          console.log('Generated email:', dynamicEmail);
+          
+    cy.wrap(dynamicEmail).as('generatedEmail');
+
+    cy.get(SELECTORS.emailAddress)
+              .click()
+              .type(dynamicEmail);
+    });
+
+    cy.get(SELECTORS.companyName)
+         .click()
+         .type(TEST_DATA.company);
+
+    cy.get(SELECTORS.leadSource)
+         .click()
+         .type(TEST_DATA.lead);
+
+    cy.get(SELECTORS.submitButton)
+         .click();
+    
+         cy.wait(3000);
+
+  };
+
+  const createPropertyProfile = () => {
+
+    cy.get(SELECTORS.addManually)
+         .contains(MESSAGES.manualAddressButton)
+         .click();
+
+    cy.generateRandomWords(5).then((addressWords) => {
+          const propertyAddress = addressWords.join(' ');
+          console.log('Generated property address:', propertyAddress);
+          
+    cy.get(SELECTORS.propertyAddress)
+          .click()
+          .type(propertyAddress);
+      });
+
+    cy.get(SELECTORS.propertyCity)
+          .click()
+          .type(TEST_DATA.city);
+
+    cy.get(SELECTORS.propertyState)
+          .click()
+          .type(TEST_DATA.state);
+
+    cy.get(SELECTORS.propertyZipCode)
+          .click()
+          .type(TEST_DATA.zipCode)
+    
+    cy.get(SELECTORS.submitButton)
+          .click();
+
+    cy.get(SELECTORS.propertyLocation)
+          .contains(MESSAGES.locationSelector)
+          .click({ force: true });
+
+    cy.get(SELECTORS.propetyOption)
+         .eq(7)
+         .click();
+
+    cy.get(SELECTORS.propetyName)
+          .click()
+          .type(TEST_DATA.nameProperty);
+        
+    cy.get(SELECTORS.submitButton)
+          .click()
+
+    cy.get(SELECTORS.successToast)
+          .should('be.visible')
+          .and('contain', MESSAGES.successfulToast);
+
+    cy.wait(3000);
+    
+  };
+
+
+  const verifyCustomerCreation = () => {
+     cy.get('@generatedEmail').then((email) => {
+     cy.get(SELECTORS.tableRow)
+        .first()
+        .find('td')
+        .eq(2)
+        .should('contain.text', email);
+});
+  };
+  const deleteCustomer = () => {
+
+    cy.get(SELECTORS.tableRow)
+  .first()
+  .within(() => {
+    cy.get(SELECTORS.deleteMenu).click({ force: true });
+  });
+
+  cy.get(SELECTORS.menuItem)
+  .contains(MESSAGES.deleteOption)
+  .click({force: true})
+  cy.get('[role="dialog"]')                  // modal container, adjust if needed
+  .within(() => {
+    cy.get('button')
+      .contains(MESSAGES.deleteOption)
+      .click();
+      cy.wait(1000)
+      });
+      navigateToCustomerPage();
+      cy.get('@generatedEmail').then((email) => {
+            cy.contains(email).should('not.exist'); // simple and effective
+          });
+
+  }
 describe('Customers Test Cases', () => {
     beforeEach(() => {
         cy.Login_UI();
     });
-it('Create a Customer', () => {
+it.only('Create a Customer & Delete it', () => {
 navigateToCustomerPage();
+createCustomerProfile();
+createPropertyProfile();
+verifyCustomerCreation();
+  deleteCustomer();
 })
   })
